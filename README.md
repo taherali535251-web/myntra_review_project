6c15cfd2a9983fe571a22b3f2558bd555a7c7363
Taherali <taherali.535251@gmail.com>
2026-07-04 23:23:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#myntra Review Scrapper project
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='Taherali',
+    author_email='taherali.535251@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
